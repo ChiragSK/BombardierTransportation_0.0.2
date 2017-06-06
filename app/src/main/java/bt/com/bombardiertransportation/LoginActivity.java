5817cf4dc290bc248d991cf1ec02ab9c5bdd2ac4
Chirag <chirag_khangani@yahoo.com>
2017-06-07 01:01:34 +0530

    Initial commit

---
+++ app/src/main/java/bt/com/bombardiertransportation/LoginActivity.java
@@ -0,0 +1,81 @@
+package bt.com.bombardiertransportation;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class LoginActivity extends AppCompatActivity{
+
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+
+
+    // UI references.
+    public EditText userId;
+    public EditText password;
+    public TextView txt1;
+    Button b1;
+    int counter = 3;
+    final Context context = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        userId = (EditText)this.findViewById(R.id.edit_usernameId);
+
+        password = (EditText)this.findViewById(R.id.edit_passwordID);
+        txt1 = (TextView)findViewById(R.id.username_id);
+
+        final Button b1 = (Button)this.findViewById(R.id.LoginButtonID);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(userId.getText().toString().equals("admin") &&
+                        password.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                    Intent intent2=new Intent(context,GenerateQRforDevice.class);
+
+                    context.startActivity(intent2);
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                            txt1.setVisibility(View.VISIBLE);
+                    txt1.setBackgroundColor(Color.RED);
+                    counter--;
+                    txt1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        b1.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+    }
+
+
+}
+
